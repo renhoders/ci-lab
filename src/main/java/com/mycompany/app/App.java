f29cc00261d77f2242aae54dd8102fb93736f588
renhoders <lorenzoavv@gmail.com>
2022-10-19 17:54:18 -0300

    cambios en codigo App

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,26 @@ package com.mycompany.app;
  *
  */
 public class App {
+  
+  
+  public static boolean esTrianguloPitagoro(int a, int b, int c) {
+  
+    return ((Math.pow(c,2) == (Math.pow(a,2)) Math.pow(b,2)));
+    
+    return false;
+  }
+  
   public static String triangulo(int a, int b, int c) {
+    
+    
+    if (Boolean.TRUE.equals(esTrianguloPitagoro(a,b,c))) {
+       return "es un triangulo pitagoro"
+    }
+    
+    if ((a != b) && (b != c ) && (a != c)) {
+      return " es un triangulo escaleno"
+    }
+      
 
     if (!(a + b > c && a + c > b && b + c > a)) {
       return "error no es un triangulo";
